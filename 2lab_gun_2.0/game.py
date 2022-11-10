a634918d354153e9214256a5346f9930da9978cf
Yaroslav Orlov <orlov.iaa@phystech.edu>
2022-11-11 00:54:58 +0300

    Шары появляются, но им одиноко

--- 2lab_gun_2.0/game.py
+++ 2lab_gun_2.0/game.py
@@ -1,5 +1,7 @@
 import pygame
 from config import *
+import projectile
+from random import randint
 
 
 class Game:
@@ -12,6 +14,10 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.font = pygame.font.SysFont("Verdana", 40)
         self.clock = pygame.time.Clock()
+        self.enemy_period = PERIODS
+        self.enemy_time = {}
+        for enemy in self.enemy_period.keys():
+            self.enemy_time[enemy] = randint(0, self.enemy_period[enemy])
         #self.canon =
 
     def draw(self):
@@ -30,9 +36,27 @@ class Game:
             self.draw()
             self.clock.tick(FPS)
             self.event_processing()
-
+            self.move()
+            self.spawn()
 
     def event_processing(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.finished = True
+
+    def move(self):
+        for proj in self.projectiles:
+            proj.move()
+        for enemy in self.enemies:
+            enemy.move()
+
+    def spawn(self):
+        if self.enemy_time['Ball'] < self.enemy_period['Ball']:
+            self.enemy_time['Ball'] += 1
+        else:
+            self.enemy_time['Ball'] = 0
+            self.enemies.append(projectile.Ball(self.screen,
+                                                x=randint(0, WIDTH), y=randint(0, HEIGHT * 2 / 3),
+                                                vx=randint(-VELOCITY, VELOCITY), vy=randint(-VELOCITY, VELOCITY),
+                                                r=randint(30, 60)))
+
--- 2lab_gun_2.0/projectile.py
+++ 2lab_gun_2.0/projectile.py
@@ -1,11 +1,21 @@
-class Projectile:
-    def __init__(self, screen: pygame.Surface, x=40, y=450, vx=0, vy=0, r=15):
-        """ Конструктор класса ball
+from random import choice, randint
+from config import *
+import pygame
 
-        Args:
-        x - начальное положение мяча по горизонтали
-        y - начальное положение мяча по вертикали
-        """
+class Projectile:
+    def __init__(self, screen: pygame.Surface, x, y, vx, vy):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+
+    def draw(self):
+        pass
+
+
+class Ball(Projectile):
+    def __init__(self, screen: pygame.Surface, x=40, y=450, vx=0, vy=0, r=15):
         self.screen = screen
         self.x = x
         self.y = y
@@ -13,5 +23,28 @@ class Projectile:
         self.vx = vx
         self.vy = vy
         self.color = choice(GAME_COLORS)
-        self.live = 30
 
+    def draw(self):
+        pygame.draw.circle(
+            self.screen,
+            self.color,
+            (self.x, self.y),
+            self.r,
+            width=1
+        )
+
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+        if self.x > WIDTH - self.r:
+            self.x = self.x = 2 * WIDTH - 2 * self.r - self.x
+            self.vx = -self.vx
+        if self.x < self.r:
+            self.x = 2 * self.r - self.x
+            self.vx = -self.vx
+        if self.y > (HEIGHT * 2 / 3) - self.r:
+            self.y = 2 * (HEIGHT * 2 / 3) - 2 * self.r - self.y
+            self.vy = -self.vy
+        if self.y < self.r:
+            self.y = 2 * self.r - self.y
+            self.vy = -self.vy
